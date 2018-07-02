ca81881270e5ce3214e49cc8dbcd88703a0125eb
mac <mac@macs-MacBook-Pro.local>
2018-07-02 19:30:12 -0300

    findmovie

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import Detail from './pages/Detail'
+import { NotFound } from './pages/NotFound'
+
+import './App.css';
+import 'bulma/css/bulma.css'
+
+class App extends Component {
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path = '/' component = {Home} />
+          <Route exact path = '/detail/:id' component = {Detail} />
+          <Route component = {NotFound} />
+        </Switch>
+      </div>
+    )
+  }
+}
+
+export default App;
