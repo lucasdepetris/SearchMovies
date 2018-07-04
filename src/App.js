7fbf897a4e4d1d9f3c6cb7295cb6328985f5e75d
lucas <lucas.depetris@awsoftware.com.ar>
2018-07-04 10:51:44 -0300

    - Google places autocomplete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Detail from './pages/Detail'
 import { NotFound } from './pages/NotFound'
+import Places from './pages/Places'
 
 import './App.css';
 import 'bulma/css/bulma.css'
@@ -15,7 +16,7 @@ class App extends Component {
     return (
       <div className="App">
         <Switch>
-          <Route exact path = '/' component = {Home} />
+          <Route exact path = '/' component = {Places} />
           <Route exact path = '/detail/:id' component = {Detail} />
           <Route component = {NotFound} />
         </Switch>
