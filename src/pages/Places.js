157a7d6de648151e892eb1ced3682f92ddc7f521
lucas <lucas.depetris@awsoftware.com.ar>
2018-07-04 11:39:14 -0300

    - Segunda libreria

--- src/pages/Places.js
+++ src/pages/Places.js
@@ -1,64 +1,72 @@
 import React, {Component} from "react"
-import GoogleMapLoader from "react-google-maps-loader"
-import GooglePlacesSuggest from "react-google-places-suggest"
+import PlacesAutocomplete from 'react-places-autocomplete';
  
+import {
+    geocodeByAddress,
+    geocodeByPlaceId,
+    getLatLng,
+  } from 'react-places-autocomplete';
+
 const MY_API_KEY = "AIzaSyCt8ZWpFeRniRAWxm_zb2XWCaNYcpiGri0" // fake
- 
-export default class GoogleSuggest extends Component {
-    state = {
-        search: "",
-        value: "",
+
+export default class Places extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { address: '' };
     }
- 
-    handleInputChange = e => {
-        this.setState({search: e.target.value, value: e.target.value})
-    }
- 
-    handleSelectSuggest = (geocodedPrediction, originalPrediction) => {
-        console.log(geocodedPrediction, originalPrediction) // eslint-disable-line
-        console.log(geocodedPrediction.geometry.bounds.b.b)
-        console.log(geocodedPrediction.geometry.bounds.f)
-        this.setState({search: "", value: geocodedPrediction.formatted_address})
-    }
- 
+   
+    handleChange = address => {
+      this.setState({ address });
+    };
+   
+    handleSelect = address => {
+      geocodeByAddress(address)
+        .then(results => getLatLng(results[0]))
+        .then(latLng => console.log('Success', latLng))
+        .catch(error => console.error('Error', error));
+    };
+   
     render() {
-        const {search, value} = this.state
-        return (
-            <GoogleMapLoader
-                params={{
-                    key: MY_API_KEY,
-                    libraries: "places,geocode",
-                }}
-                render={googleMaps =>
-                    googleMaps && (
-                        <GooglePlacesSuggest
-                            googleMaps={googleMaps}
-                            autocompletionRequest={{
-                                input: search,
-                                // Optional options
-                                // https://developers.google.com/maps/documentation/javascript/reference?hl=fr#AutocompletionRequest
-                            }}
-                            // Optional props
-                            onSelectSuggest={this.handleSelectSuggest}
-                            textNoResults="My custom no results text" // null or "" if you want to disable the no results item
-                            customRender={prediction => (
-                                <div className="customWrapper">
-                                    {prediction
-                                        ? prediction.description
-                                        : "My custom no results text"}
-                                </div>
-                            )}
-                        >
-                            <input
-                                type="text"
-                                value={value}
-                                placeholder="Search a location"
-                                onChange={this.handleInputChange}
-                            />
-                        </GooglePlacesSuggest>
-                    )
-                }
-            />
-        )
+      return (
+          
+        <PlacesAutocomplete
+          value={this.state.address}
+          onChange={this.handleChange}
+          onSelect={this.handleSelect}
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+            <div>
+              <input
+                {...getInputProps({
+                  placeholder: 'Search Places ...',
+                  className: 'location-search-input',
+                })}
+              />
+              <div className="autocomplete-dropdown-container">
+                {loading && <div>Loading...</div>}
+                {suggestions.map(suggestion => {
+                  const className = suggestion.active
+                    ? 'suggestion-item--active'
+                    : 'suggestion-item';
+                  // inline style for demonstration purpose
+                  const style = suggestion.active
+                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                  return (
+                    <div
+                      {...getSuggestionItemProps(suggestion, {
+                        className,
+                        style,
+                      })}
+                    >
+                      <span>{suggestion.description}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </PlacesAutocomplete>
+      );
     }
-}
+  } 
